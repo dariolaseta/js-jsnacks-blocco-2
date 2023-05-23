fe6a05e9da197dfbc83e649ff58a75c19c566b88
dariolaseta <dario.laseta@gmail.com>
2023-05-23 12:35:46 +0200

    added text to script

--- snack-plus/script.js
+++ snack-plus/script.js
@@ -1,14 +1,15 @@
 let list = document.querySelector("ul");
-let listItems = ["Uova", "Tonno", "Pomodori", "Pomodori", "Pomodori", "Pomodori", "Pomodori", "Pomodori", "Pomodori", "Pomodori"];
 
 
 function createList(){
     let listElement = document.createElement("li");
+    listElement.innerHTML = "Test";
+
     return listElement;
 }
 
 for(let i = 0; i < listItems.length; i++){
     let generatedList = createList();
-
+    
     list.appendChild(generatedList);
 }
