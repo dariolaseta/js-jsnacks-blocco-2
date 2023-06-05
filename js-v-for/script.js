e9ca1a51403263bc80c21d345596b7f679b6c8f0
dariolaseta <dario.laseta@gmail.com>
2023-06-05 11:49:46 +0200

    removed unused array

--- js-v-for/script.js
+++ js-v-for/script.js
@@ -3,8 +3,7 @@ const { createApp } = Vue;
 createApp({
     data(){
         return{
-            nameArray: [{ name: 'Ginetta' }, { name: 'Gina' }, { name: 'Ginetto' }, { name: 'Gino' }],
-            numberArray: [1, 2, 5, 6, 7, 1, 0, 2, 4, 5]
+            nameArray: [{ name: 'Ginetta' }, { name: 'Gina' }, { name: 'Ginetto' }, { name: 'Gino' }]
         }
     }
 }).mount("#for-cicle");
